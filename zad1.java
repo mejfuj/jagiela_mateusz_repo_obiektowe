9378e288822113df8d9b48f91ad0dd2c9060f160
mejfuj <jagielamati@gmail.com>
2021-02-23 09:31:28 +0100

    Create zad1.java

---
+++ zad1.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+class Tablice1{
+    public static void main(String[] args){
+    
+        String tab2 [] = {"Jan","Mariusz","Mariola","Kacper","Michal","Katarzyna"};
+        for(int i=0;i<tab2.length;i++){
+            System.out.println(i+" -> "+tab2[i]);
+        }
+        
+        String oceny [];
+        for(int i = 0; i < 3; i++) {
+            double rand = Math.random(); 
+            System.out.println("ocena: " + rand);
+        }
+
+        //Wyświetlanie elementów tablicy -> pętla FOR
+        /*for (int i=0;i<tab2.length;i++){
+            System.out.println(i+" - "+tab2[i]);
+        }*/
+    }
+}
