a189e12261dc6dbce0343519ae47b6097589e17e
Mateusz <77609334+mejfuj@users.noreply.github.com>
2021-03-01 09:08:26 +0100

    Stworzenie tablicy 2wym.

--- Praca na lekcji 01.03.2021/TablicaDwuwymiarowa.java
+++ Praca na lekcji 01.03.2021/TablicaDwuwymiarowa.java
@@ -1,8 +1,18 @@
 
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
+    //Commit: Przypomnienie jak tworzy sie tablice 1wymiarowa    
         int [] tab = new int[5];
+        //przypisanie do pierwszego elementu tablicy [index = 0] wartosci 40
         tab[0] = 40;
-        tab[1] = "Jeden"; //error
+        //tak nie mozna bo error wyskkauje ze stringa nie mozna na inta zrobic
+            //tab[1] = "Jeden"; //error
+        //wyświetlenie elementow tablicy:
+        for(int i=0;i<tab.length()i++){
+            System.out.println(tab[i]);
+        }    
+    //commit: Przypomnienie jak robic tab 1wym.
+        int[][] tab2b = new int[5][2]; 
+    //commit: Stworzenie tablicy 2wymiarowej     
     }
 }
