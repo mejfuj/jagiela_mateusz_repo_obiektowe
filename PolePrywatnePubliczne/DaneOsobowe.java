6833450afeeba4f8de9c3380ac22def19a15fce9
mejfuj <jagielamati@gmail.com>
2021-03-16 10:03:51 +0100

    16.03.2021

---
+++ PolePrywatnePubliczne/DaneOsobowe.java
@@ -0,0 +1,7 @@
+
+class DaneOsobowe{
+    private int pesel = "12345678901";
+    public int wiek = 20;
+    public int wzrost = 177;
+    public int waga = 88;
+}
---
+++ PolePrywatnePubliczne/PolePrywatnePubliczne.java
@@ -0,0 +1,13 @@
+
+class PolePrywatnePubliczne {
+    //Definiowanie zmiennych / obiektów globalnie
+    DaneOsobowe dos;
+    public static void main(String [] args){
+        //Stworzenie obiektu
+        dos = new DaneOsobowe(); //od tego momentu moge uzywac nazwy: dos
+        System.out.println(dos.pesel);
+    }
+
+
+
+}
