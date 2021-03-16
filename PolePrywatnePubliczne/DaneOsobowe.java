e31d86daa2d47874db8724f058a025abcaee9582
mejfuj <jagielamati@gmail.com>
2021-03-16 10:31:08 +0100

    16.03.2021

--- PolePrywatnePubliczne/DaneOsobowe.java
+++ PolePrywatnePubliczne/DaneOsobowe.java
@@ -1,7 +1,25 @@
-
 class DaneOsobowe{
-    private int pesel = "12345678901";
-    public int wiek = 20;
-    public int wzrost = 177;
-    public int waga = 88;
+	private String pesel = "12345678901";
+	public int wiek = 20;
+	public int wzrost = 177;
+	public int waga = 88;
+	//zmienne do adresu zamieszkania!!!
+	String miasto = "Warszawa";
+	String ulica = "Pilsudskiego";
+	int nr_domu = 33;
+	int nr_mieszkania = 3;
+	String kod = "12-345";
+	
+	
+	public double mybmi(){
+		//Rzutowanie int na double:  (double)wzrost
+		System.out.println("Osoba o peselu: "+pesel);
+		return waga/((double)wzrost/100*(double)wzrost/100);
+	}
+	//metoda wyświetlająca adres zamieszkania!!!
+	public void myAdress(){
+		System.out.println("Moja wizytówka: "
+		+"\nMiasto: "+miasto+" "+kod
+		+"\nUlica: "+ulica+" "+nr_domu+"/"+nr_mieszkania);
+	}
 }
--- PolePrywatnePubliczne/PolePrywatnePubliczne.java
+++ PolePrywatnePubliczne/PolePrywatnePubliczne.java
@@ -1,13 +1,15 @@
-
-class PolePrywatnePubliczne {
-    //Definiowanie zmiennych / obiektów globalnie
-    DaneOsobowe dos;
-    public static void main(String [] args){
-        //Stworzenie obiektu
-        dos = new DaneOsobowe(); //od tego momentu moge uzywac nazwy: dos
-        System.out.println(dos.pesel);
-    }
-
-
-
-}
+class PolaPrywatnePubliczne{
+	//Definiowanie zmiennych/obiektów globalnie!!!
+	static DaneOsobowe dos;
+	
+	public static void main(String [] args){
+		//Stworzenie obiektu
+		dos = new DaneOsobowe(); //od tego meomentu mogę używać nazwy: dos
+		//System.out.println("1 os BMI: "+ dos.mybmi()   );
+		dos.wzrost = 192;
+		dos.waga = 94;
+		//System.out.println("2 os BMI: "+ dos.mybmi()   );
+		dos.myAdress();
+	}
+	
+} 
